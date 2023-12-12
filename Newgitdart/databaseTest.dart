8a039141b5dfb47cf856533adccd80d3c431e9d0
jake aleta <simdual4@gmail.com>
2023-12-12 22:49:36 +0800

    Map with if else input

---
+++ Newgitdart/databaseTest.dart
@@ -0,0 +1,8 @@
+// import 'package:sqflite/sqflite.dart';
+
+
+// class SqlDb {
+//   initialDB() async {
+//     String databasepath = await getDatabasesPath() ;
+//   }
+// }
--- Newgitdart/testgitrun.dart
+++ Newgitdart/testgitrun.dart
@@ -1,32 +1,20 @@
+import 'dart:io';
+
 void main() {
-  Map<String, double> groceryList = {
-    'Bread': 2.50,
-    'Milk': 1.80,
-    'Eggs': 2.00,
-    'Cheese': 4.50,
-    'Fruits': 6.00,
-    'Vegetables': 3.50,
-    'Chicken': 8.00,
-    'Rice': 1.50,
-    'Pasta': 2.00,
-    'Cereal': 3.20,
-    'Snacks': 4.00,
-    'Water': 0.80,
-    'Juice': 2.50,
-    'Coffee': 5.00,
-    'Tea': 3.00,
-    'Spices': 2.50,
-    'Condiments': 2.00,
-    'Toilet Paper': 1.50,
-    'Paper Towels': 2.00,
-    'Soap': 1.00,
-    'Shampoo': 3.50,
-    'Toothpaste': 2.00,
-    'Dish Soap': 1.80,
+  Map<String, double> ListItem = {
+    '1.Milk Tea': 5,
+    '2.Coffee Tea': 10,
+    '3.Turon': 20
   };
 
-  print('Grocery List with Prices:');
-  groceryList.forEach((item, price) {
-    print(' -$item: \$${price.toStringAsFixed(2)}');
+  ListItem.forEach((item, price) {
+    print('-$item:\$${price.toStringAsFixed(2)}');
   });
+  print('What You want to Buy.... Choose a Number');
+
+  var input = stdin.readLineSync();
+
+  if (input == 1) {
+    print('Your order is $ListItem');
+  }
 }
